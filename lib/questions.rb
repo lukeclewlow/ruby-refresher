a917eb2f9402faf76c6ae9d23473e1f001aad338
Luke Clewlow <luke.clewlow@gmail.com>
2015-02-09 12:28:55 +0000

    25 down

--- lib/questions.rb
+++ lib/questions.rb
@@ -49,7 +49,7 @@ end
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
-	array.sort { |x, y| x(-1) <=> y(-1) }
+	array.sort_by { |letter| letter[-1]}
 end
 
 # cut strings in half, and return the first half, e.g.
